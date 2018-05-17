02ec8665e288dde51c9ea0a9f9c4045fb351412d
Sajwanankita <31779268+Sajwanankita@users.noreply.github.com>
2018-05-17 16:35:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,14 +12,12 @@ const app = express_1.default();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 
-app.use(function  (req,  res,  next) {
-     res.header("Access-Control-Allow-Origin",  "*");
-     res.setHeader('Access-Control-Allow-Methods',  'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-     res.header("Access-Control-Allow-Headers",  "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (req, res, next) {
+     res.header("Access-Control-Allow-Origin", "*");
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
      next();
 }); 
- 
-
 
 app.use('/', api_1.default);
 app.use((req, res) => {
