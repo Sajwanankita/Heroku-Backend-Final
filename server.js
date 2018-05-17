e546fc8fb83550983c1bff8297ae0d4e309c1b99
Sajwanankita <ankitasajwan123@gmail.com>
2018-05-17 16:23:15 +0530

    init

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ const api_1 = __importDefault(require("./routes/api"));
 const app = express_1.default();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+
+app.use(function  (req,  res,  next) {
+     res.header("Access-Control-Allow-Origin",  "*");
+     res.setHeader('Access-Control-Allow-Methods',  'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+     res.header("Access-Control-Allow-Headers",  "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+}); 
+ 
+
+
 app.use('/', api_1.default);
 app.use((req, res) => {
     res.status(404).send('Not Found');
